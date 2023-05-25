86b3ce1688b955adf136b6c20ab659c3b56c1017
NicolaVilla <nicovillarpando@outlook.es>
2023-05-24 21:02:26 -0300

    feat: :sparkles: Crea funcion leer_csv
    funcion que permite obtener el contenido de un archivo csv y lo convierte en un diccionario

---
+++ main.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+ruta_csv = ".archivos/cities.csv"
+ruta_json = ".archivos/cities.json"
+
+#Funcion para leer y obtener la data del csv
+def leer_csv(ruta):
+    if(ruta[-4:]!=".csv"):
+        print("No es un archivo .csv")
+        return
+    with open(ruta,"r") as file:
+        reader = csv.DictReader(file, skipinitialspace=True) #almacena el contenido del csv como diccionario
+        fields = reader.fieldnames #almacena las claves del diccionario
+        data = [] 
+        for row in reader:
+            data.append({fields[i]:row[fields[i]] for i in range(len(fields))})
